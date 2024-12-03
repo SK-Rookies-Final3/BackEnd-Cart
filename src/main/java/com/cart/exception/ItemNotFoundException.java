6a2ac7a385e46c37743bd7d832113dee89d6f215
woo <clwkwhdk@gmail.com>
2024-12-03 10:21:11 +0900

    styleÂ: 파일 잘못옮김

--- src/main/java/com/cart/exception/ItemNotFoundException.java
+++ src/main/java/com/cart/exception/ItemNotFoundException.java
@@ -1,7 +1,7 @@
 package com.cart.exception;
 
-public class CustomCartNotFoundException extends RuntimeException {
-    public CustomCartNotFoundException(String message) {
+public class ItemNotFoundException extends RuntimeException {
+    public ItemNotFoundException(String message) {
         super(message);
     }
 }
