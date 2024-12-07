33c5ec0f5aebd6e74019fcfdc771b82c31a3bf76
woo <clwkwhdk@gmail.com>
2024-12-07 14:24:51 +0900

    "chore: 로컬"

--- src/main/java/com/cart/CartApplication.java
+++ src/main/java/com/cart/CartApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
-//@EnableDiscoveryClient
+@EnableDiscoveryClient
 public class CartApplication {
 
 	public static void main(String[] args) {
